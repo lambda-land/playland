0285e2374b2ad391691d062776e328181d819fc8
Iain Moncrief <Iainmoncrief@gmail.com>
2021-10-28 13:34:13 -0700

    added line

--- src/main.js
+++ src/main.js
@@ -154,18 +154,7 @@ self.MonacoEnvironment = {
 
 monaco.editor.create(document.getElementById('container'), {
 	value: [
-		'from banana import *',
-		'',
-		'class Monkey:',
-		'	# Bananas the monkey can eat.',
-		'	capacity = 10',
-		'	def eat(self, N):',
-		"		'''Make the monkey eat N bananas!'''",
-		'		capacity = capacity - N*banana.size',
-		'',
-		'	def feeding_frenzy(self):',
-		'		eat(9.25)',
-		'		return "Yum yum"'
+        'type Option<T> = Some<T> | None<T>;'
 	].join('\n'),
 	language: 'typescript'
 });
