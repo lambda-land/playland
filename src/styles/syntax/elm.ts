b84972d349ed760f0a0d28911c6ae92eeb543441
Iain Moncrief <Iainmoncrief@gmail.com>
2022-03-25 13:04:35 -1000

    color themes work

--- src/styles/syntax/elm.ts
+++ src/styles/syntax/elm.ts
@@ -2,8 +2,8 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-import { languages } from 'monaco-editor';
-// import { languages } from 'monaco-editor/esm/vs/editor/editor.api.js';
+// import { languages } from 'monaco-editor';
+import { languages } from 'monaco-editor/esm/vs/editor/editor.api.js';
 // import * as monaco from 'monaco-editor/esm/vs/editor/editor.api.js';
 
 export var conf = {
